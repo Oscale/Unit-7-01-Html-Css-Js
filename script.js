d724e1b437cc355815e81209e357e70c41deae47
Oscar A.G <>
2021-05-07 22:44:57 +0000

    Hopefully fixed Github linter Errors.

--- script.js
+++ script.js
@@ -5,13 +5,13 @@ let rannum = 0
 
 function Begin () {
   inputa = document.getElementById('input').value
-  
+
   inputa = parseInt(inputa)
   rannum = parseInt(rannum)
 
-  rannum = Math.floor(Math.random() * 6 ) + 1
-  
-  if (rannum == inputa) {
+  rannum = Math.floor(Math.random() * 6) + 1
+
+  if (rannum === inputa) {
     document.getElementById('output').innerHTML = 'correct!'
   } else {
     document.getElementById('output').innerHTML = 'incorrect'
